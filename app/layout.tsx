f56c300c76d52b1eca68372959855ce36580d400
Zxhy <tlee4014@gmail.com>
2026-03-17 23:33:34 +0800

    feat: 添加坦克图标作为网站favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import { Providers } from '@/src/components/Providers'
 export const metadata: Metadata = {
   title: '坦克大战 - Battle City',
   description: '经典坦克大战 Web 复刻版',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
